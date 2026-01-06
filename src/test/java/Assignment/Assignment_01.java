2ef3b8d7bd3615c9a4e370435e922fdbc1b0be9e
Devendrab98 <devendrabaviskar81@gmail.com>
2026-01-07 00:46:14 +0530

    New changes

--- src/test/java/Assignment/Assignment_01.java
+++ src/test/java/Assignment/Assignment_01.java
@@ -51,39 +51,56 @@ public class Assignment_01 {
     @Test(priority = 2)
     public void PDF() {
 
+        // Navigate to Website
         driver.get("https://smallpdf.com/edit-pdf");
 
+        // Uplaod the file
         WebElement UploadFile = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@Class='sc-16z3mvs-2 hdkHlv']//input[@type='file']")));
 
+        // Send the file path
         UploadFile.sendKeys("D:\\dummy_pdf.pdf");
 
+        // Click on the Text option
         WebElement ClickOnAddTextbtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='fx7l8p-0 jFCdPQ']//button)[4]")));
         ClickOnAddTextbtn.click();
 
+        // Click on the Document page
         WebElement ClickOntheTextPage = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='pageWidgetContainer1']")));
         ClickOntheTextPage.click();
 
+        // Add the Api data on document page
         Actions act = new Actions(driver);
         act.sendKeys("Api Data add :" + APiData).perform();
 
 
+        // Add one sign annotation
+        // Add sign annotation
         WebElement ClickOnAddSignBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='fx7l8p-0 jFCdPQ']//button)[8]")));
         ClickOnAddSignBtn.click();
 
-
+        // Click on a new sign option
         WebElement ClickOnNewSignature = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='New signature']")));
         ClickOnNewSignature.click();
 
+        // Click on the sign type
         WebElement ClickOnType = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='ng9kw0-1 dpiSLm']//button[2])")));
         ClickOnType.click();
 
+
+        // Click on Text type
         WebElement ClickOnTextFl = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='signature-text-input']")));
         ClickOnTextFl.sendKeys("Devendra");
 
+        // Click on create button
         WebElement ClickOnCreate = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Create']")));
         ClickOnCreate.click();
 
+        // Click on document page to add sign
         ClickOntheTextPage.click();
+
+        // Click on download
+        WebElement DowloadPdf = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Download']")));
+        DowloadPdf.click();
 
     }
 
